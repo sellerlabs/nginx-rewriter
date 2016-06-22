b420f07137843f1ca1e78a0a0448adce4a3cd45e
Austin Lane <austin@roundsphere.com>
2016-06-22 15:23:06 -0400

    WIP: HTTPS Implementation with Let's Encrypt

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 FROM nginx
 MAINTAINER Brian Whigham <oobx@itmonger.com>
 RUN \
+	echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list.d/jessie-backports.list && \
   apt-get update && \
-  apt-get install -y python python-yaml 
+  apt-get install -y python python-pip python-yaml && \
+  apt-get install -y certbot -t jessie-backports && \
+  pip install cffi
+
+RUN \
+	mkdir -p /var/www/nginx-rewriter && \
+	chmod -R 777 /var/www/nginx-rewriter
+
 RUN \
   rm -f /var/log/nginx/* && \
   mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/zzz-default.conf && \
--- start.py
+++ start.py
@@ -6,33 +6,76 @@ import os
 import sys
 from subprocess import PIPE, Popen
 
-home = os.path.expanduser("~")
-confd = '/etc/nginx/conf.d/'
+class NginxRewriter(object):
+    def __init__(self):
+        self.home = os.path.expanduser("~")
+        self.confd = '/etc/nginx/conf.d/'
+        self.webroot = '/var/www/nginx-rewriter/'
+        config_url = os.getenv('CONFIG_URL',"https://raw.githubusercontent.com/sellerlabs/nginx-rewriter/master/rewrite-rules.example.yaml")
+        response = urllib2.urlopen(config_url)
+        self.config = load(response)
+        self.email_addr = self.config['email']
+        self.target_domains = self.config['target_domains']
 
-print "preparing to start nginx-rewriter ..."
-config_url = os.getenv('CONFIG_URL',"https://raw.githubusercontent.com/sellerlabs/nginx-rewriter/master/rewrite-rules.example.yaml")
-print "CONFIG_URL: " + config_url
+        self.gen_config()
 
-response = urllib2.urlopen(config_url)
-config = load(response)
+    def gen_cert(self, redir_domains):
+        print redir_domains
+        command = Popen(["/usr/bin/certbot","certonly","--staging","--dry-run","--email",self.email_addr,"-n","--agree-tos","-t","--standalone","--standalone-supported-challenges","http-01","-d",redir_domains])
+        command.wait()
 
-print "loading domains"
-for domain in config:
-  print domain
-  c = open(confd + domain + ".conf", 'w')
-  c.write('server {\n  server_name ' + domain + ';\n')
-  print "adding rules"
-  for rule in config[domain]['rewriterules']:
-    print rule
-    c.write('  rewrite ' + rule + '\n')
-  c.write('}')
-  c.close
+    def gen_config(self):
+        print "loading domains"
 
-print "testing config..."
-sys.stdout.flush()
-p = Popen(["/usr/sbin/nginx","-t"], stderr=sys.stderr, stdout=sys.stdout)
-p.wait()
-print "starting nginx in the foreground"
-sys.stdout.flush()
-p = Popen(["/usr/sbin/nginx","-g", "daemon off;"], stderr=sys.stderr, stdout=sys.stdout)
-p.wait()
+        for target_domain in self.target_domains:
+            redir_domains = self.target_domains[target_domain]['redir_domains']
+            redir_domains_spaces = "  ".join(redir_domains)
+            redir_domains_csv = ",".join(redir_domains)
+
+            p = Popen(["mkdir",self.webroot+target_domain])
+            p.wait()
+
+            print self.webroot+target_domain
+            self.gen_cert(redir_domains_csv)
+
+            c = open(self.confd + target_domain + ".conf", 'w')
+
+            print "Nginx config %s" % target_domain
+            server_conf = """
+server {
+    listen 80;
+    server_name %s;
+    location / {
+        return 301 https://$host$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name %s;
+    return 301 https://%s$request_uri;
+
+    ssl_certificate /etc/letsencrypt/live/%s/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/%s/privkey.pem;
+}
+            """ % (redir_domains_spaces, redir_domains_spaces, target_domain, redir_domains[0], redir_domains[0])
+
+            print server_conf
+
+            c.write(server_conf)
+            c.close
+
+        self.start_nginx()
+
+    def start_nginx(self):
+        print "testing config..."
+        sys.stdout.flush()
+        p = Popen(["/usr/sbin/nginx","-t"], stderr=sys.stderr, stdout=sys.stdout)
+        p.wait()
+        print "starting nginx in the foreground"
+        sys.stdout.flush()
+        p = Popen(["/usr/sbin/nginx","-g", "daemon off;"], stderr=sys.stderr, stdout=sys.stdout)
+        p.wait()
+
+if __name__ == "__main__":
+    NginxRewriter()
